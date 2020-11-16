627cd0441f82f8041bb032341409a108b99085ba
Joshua Hawkins <josh.r.hawk@gmail.com>
2020-11-16 15:31:49 -0500

    match float64 type

--- rust/perspective/arrow-data-slice/src/arrow.rs
+++ rust/perspective/arrow-data-slice/src/arrow.rs
@@ -9,6 +9,7 @@
 use std::str;
 use std::io::Cursor;
 use arrow::error::{ArrowError, Result};
+use arrow::array::{Float64Array};
 use arrow::ipc::reader::{StreamReader};
 use arrow::record_batch::RecordBatch;
 use arrow::datatypes::Schema;
@@ -26,6 +27,7 @@ pub fn load_arrow_stream(buffer: Box<[u8]>) {
     let cursor = Cursor::new(buffer);
     let reader = StreamReader::try_new(cursor).unwrap();
     let schema = reader.schema();
+    
     let mut batches: Vec<RecordBatch> = Vec::new();
     log(format!("Schema from Rust StreamReader: {}", schema).as_str());
 
@@ -41,6 +43,22 @@ pub fn load_arrow_stream(buffer: Box<[u8]>) {
     for b in batches {
         let num_columns: usize = b.num_columns();
         let num_rows: usize = b.num_rows();
+
+        for i in 0..num_columns {
+            let col = b.column(i);
+            let call_type = col.data_type();
+
+            match call_type {
+                Float64 => {
+                    if let Some(result) = col.as_any().downcast_ref::<Float64Array>() {
+                        let slice = result.value_slice(0,result.len());
+                        for j in slice {
+                            log(format!("{}", j).as_str());
+                        }
+                    }
+                }
+            }
+        }
         log(format!("{} x {}", num_columns, num_rows).as_str());
     }
 }
