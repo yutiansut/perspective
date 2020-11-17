1a5b2f2773c9cbedd510e0ad24c1ed348dc26f12
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-11-16 22:50:38 -0500

    WIP: match based on type, but does not compile yet

--- rust/perspective/arrow-data-slice/src/arrow.rs
+++ rust/perspective/arrow-data-slice/src/arrow.rs
@@ -6,13 +6,16 @@
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
+use std::any::Any;
+use std::sync::Arc;
 use std::str;
 use std::io::Cursor;
+use std::collections::HashMap;
 use arrow::error::{ArrowError, Result};
-use arrow::array::{Float64Array};
+use arrow::array::*;
+use arrow::datatypes::Schema;
 use arrow::ipc::reader::{StreamReader};
 use arrow::record_batch::RecordBatch;
-use arrow::datatypes::Schema;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -22,45 +25,80 @@ extern "C" {
     fn log(s: &str);
 }
 
+pub struct ArrowAccessor<'a> {
+    data: HashMap<String, Box<&'a dyn Array>>
+}
+
 /// Load an arrow binary in stream format.
 pub fn load_arrow_stream(buffer: Box<[u8]>) {
     let cursor = Cursor::new(buffer);
     let reader = StreamReader::try_new(cursor).unwrap();
-    let schema = reader.schema();
-    
-    let mut batches: Vec<RecordBatch> = Vec::new();
-    log(format!("Schema from Rust StreamReader: {}", schema).as_str());
+    let mut accessors: Vec<Box<ArrowAccessor>> = Vec::new();
 
     reader.for_each(|batch| {
         match batch {
             Ok(val) => {
-                batches.push(val);
-            },
-            Err(err) => log(format!("{}", err).as_str())
+                accessors.push(convert_record_batch(val))
+            }, Err(err) => log(format!("{}", err).as_str())
         }
     });
+}
 
-    for b in batches {
-        let num_columns: usize = b.num_columns();
-        let num_rows: usize = b.num_rows();
+pub fn convert_record_batch<'a>(batch: RecordBatch) -> Box<ArrowAccessor<'a>> {
+    let schema = batch.schema();
+    let num_columns: usize = batch.num_columns();
+    let num_rows: usize = batch.num_rows();
 
-        for i in 0..num_columns {
-            let col = b.column(i);
-            let call_type = col.data_type();
+    log(format!("{} x {}", num_columns, num_rows).as_str());
 
-            match call_type {
-                Float64 => {
-                    if let Some(result) = col.as_any().downcast_ref::<Float64Array>() {
-                        let slice = result.value_slice(0,result.len());
-                        for j in slice {
-                            log(format!("{}", j).as_str());
-                        }
-                    }
+    let mut converted_data: HashMap<String, Box<&dyn Array>> = HashMap::new();
+
+    for i in 0..num_columns {
+        let col = batch.column(i);
+        let col_any = col.as_any();
+
+        let name: String = schema.field(i).name().clone();
+
+        if let Some(result) = col_any.downcast_ref::<Float64Array>() {
+            converted_data.insert(name, Box::new(result));
+            for j in 0..result.len() {
+                log(format!("Float64Array {}", result.value(j)).as_str());
+            }
+        } else if let Some(result) = col_any.downcast_ref::<Int64Array>() {
+            for j in 0..result.len() {
+                log(format!("Int64Array {}", result.value(j)).as_str());
+            }
+        } else if let Some(result) = col_any.downcast_ref::<Int32Array>() {
+            for j in 0..result.len() {
+                log(format!("Int32Array {}", result.value(j)).as_str());
+            }
+        } else if let Some(result) = col_any.downcast_ref::<Date32Array>() {
+            for j in 0..result.len() {
+                log(format!("Date32Array {}", result.value(j)).as_str());
+            }
+        } else if let Some(result) = col_any.downcast_ref::<TimestampMillisecondArray>() {
+            for j in 0..result.len() {
+                log(format!("TimestampMillisecondArray {}", result.value(j)).as_str());
+            }
+        } else if let Some(result) = col_any.downcast_ref::<BooleanArray>() {
+            for j in 0..result.len() {
+                log(format!("BooleanArray {}", result.value(j)).as_str());
+            }
+        } else if let Some(result) = col_any.downcast_ref::<Int32DictionaryArray>() {
+            if let Some(strings) = result.values().as_any().downcast_ref::<StringArray>() {
+                for key in result.keys() {
+                    let k = key.unwrap();
+                    log(format!("int32dict {}", strings.value(k as usize)).as_str());
                 }
             }
         }
-        log(format!("{} x {}", num_columns, num_rows).as_str());
     }
+
+    let accessor = ArrowAccessor {
+        data: converted_data
+    };
+
+    return Box::new(accessor);
 }
 
 #[wasm_bindgen]
