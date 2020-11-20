2de4abb7763305fc57b16fb8b2c3edd155b0f195
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-11-20 14:48:22 -0500

    test with pivoted columns

--- rust/perspective/arrow_accessor/src/lib.rs
+++ rust/perspective/arrow_accessor/src/lib.rs
@@ -7,18 +7,47 @@
  *
  */
 pub mod accessor;
-pub mod api;
-mod utils;
+
+use std::io::Cursor;
 
 use arrow::datatypes::{DataType, DateUnit, TimeUnit};
+use arrow::ipc::reader::StreamReader;
+use arrow::record_batch::RecordBatch;
 
 use chrono::Datelike;
 use js_sys::*;
 use wasm_bindgen::prelude::*;
 
 use crate::accessor::ArrowAccessor;
-use crate::api::load_arrow_stream;
-use crate::utils::set_panic_hook;
+
+/// Load an arrow binary in stream format.
+pub fn load_arrow_stream(buffer: Box<[u8]>) -> Box<ArrowAccessor> {
+    let cursor = Cursor::new(buffer);
+    let reader = StreamReader::try_new(cursor).unwrap();
+    let schema = reader.schema();
+
+    // Iterate over record batches, and collect them into a vector of
+    // heap-allocated batches. Do not use the `reader` after this line,
+    // as it is consumed by `.map()` below.
+    let batches = reader
+        .map(|batch| {
+            // Panic if it can't read the batch
+            Box::new(batch.unwrap())
+        })
+        .collect::<Vec<Box<RecordBatch>>>();
+
+    if let [batch] = &batches[..] {
+        Box::new(ArrowAccessor::new(batch.clone(), schema))
+    } else {
+        panic!("Arrow should only contain a single record batch.")
+    }
+}
+
+// Debug `panic!` messages inside console.error().
+pub fn set_panic_hook() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -27,31 +56,35 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn make_rust_accessor(buffer: Box<[u8]>) -> *const ArrowAccessor {
+pub fn accessor_make(buffer: Box<[u8]>) -> *mut ArrowAccessor {
     set_panic_hook();
     let accessor = load_arrow_stream(buffer);
     Box::into_raw(accessor)
 }
 
 #[wasm_bindgen]
-pub fn get_from_rust_accessor(
-    accessor: *const ArrowAccessor,
-    column_name: &str,
-    ridx: usize,
-) -> JsValue {
+pub fn accessor_get_value(accessor: *mut ArrowAccessor, column_name: &str, ridx: usize) -> JsValue {
     let accessor = unsafe { accessor.as_ref().unwrap() };
-    if !accessor.is_valid(column_name, ridx) {
-        JsValue::NULL
-    } else {
-        let schema = &accessor.schema;
+    let schema = &accessor.schema;
+
+    if schema.contains_key(column_name) {
         let dtype = &schema[column_name];
+
+        // Explicitly return JsValue::NULL here, as `Option::None` returns
+        // `undefined` by default.
         match dtype {
-            DataType::Int32 => JsValue::from(accessor.get_i32(column_name, ridx)),
+            DataType::Int32 => match accessor.get_i32(column_name, ridx) {
+                Some(num) => JsValue::from(num),
+                None => JsValue::NULL,
+            },
             DataType::Int64 => match accessor.get_i64(column_name, ridx) {
                 Some(num) => JsValue::from(num as i32),
                 None => JsValue::NULL,
             },
-            DataType::Float64 => JsValue::from(accessor.get_f64(column_name, ridx)),
+            DataType::Float64 => match accessor.get_f64(column_name, ridx) {
+                Some(num) => JsValue::from(num),
+                None => JsValue::NULL,
+            },
             DataType::Date32(DateUnit::Day) => match accessor.get_date(column_name, ridx) {
                 Some(value) => JsValue::from(Date::new_with_year_month_day(
                     value.year() as u32,
@@ -69,12 +102,30 @@ pub fn get_from_rust_accessor(
                     None => JsValue::NULL,
                 }
             }
-            DataType::Boolean => JsValue::from(accessor.get_bool(column_name, ridx).unwrap()),
-            DataType::Dictionary(ref key_type, _) => match key_type.as_ref() {
-                DataType::Int32 => JsValue::from(accessor.get_string(column_name, ridx).unwrap()),
-                dtype => panic!("Unexpected dictionary key type {:?}", dtype),
+            DataType::Boolean => match accessor.get_bool(column_name, ridx) {
+                Some(val) => JsValue::from(val),
+                None => JsValue::NULL,
             },
-            _ => panic!("Unexpected dtype: {:?}", dtype),
+            DataType::Dictionary(ref key_type, _) => match key_type.as_ref() {
+                DataType::Int32 => match accessor.get_string(column_name, ridx) {
+                    Some(val) => JsValue::from(val),
+                    None => JsValue::NULL,
+                },
+                dtype => panic!(
+                    "Unexpected dictionary key type '{:?}' in get_from_rust_accessor()",
+                    dtype
+                ),
+            },
+            _ => panic!("Unexpected dtype '{:?}' in get_from_rust_accessor()", dtype),
         }
+    } else {
+        JsValue::NULL
+    }
+}
+
+#[wasm_bindgen]
+pub fn accessor_drop(accessor: *mut ArrowAccessor) {
+    if !accessor.is_null() {
+        unsafe { Box::from_raw(accessor) };
     }
 }
